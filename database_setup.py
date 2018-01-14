18a391e93685d8f5a30603b922b0f33f1799f5fd
Tianyang Chen <tenyo.chen@gmail.com>
2018-01-14 05:51:56 -0700

    modify database_setup

--- database_setup.py
+++ database_setup.py
@@ -8,48 +8,17 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'user'
+class JOBS(Base):
+    __tablename__ = 'job'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    JobTile = Column(String(255), primary_key=True)
+    CompanyName = Column(String(255), primary_key=False)
+    FullTime = Column(Integer, nullable=False)
+    Job=Column(Integer,nullable=False)
+    Description=Column(String(10000),nullable=True)
+    JobLink=Column(String(255),nullable=False)
 
-
-class Category(Base):
-    __tablename__ = 'category'
-
-    name = Column(String(250), nullable=False, primary_key=True)
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'name': self.name
-        }
-
-
-class CategoryItem(Base):
-    __tablename__ = 'category_item'
-
-    name = Column(String(80), nullable=False, primary_key=True)
-
-    description = Column(String(250))
-    category_id = Column(String(250), ForeignKey('category.name'))
-    category = relationship(Category)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'name': self.name,
-            'description': self.description
-        }
-
-
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('mysql+pymysql://job:123@localhost:3306/JOB')
 
 
 Base.metadata.create_all(engine)
